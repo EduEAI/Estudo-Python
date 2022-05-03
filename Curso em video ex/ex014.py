7b4653f057ae2ae9f6f01ee71a5e64642930cca0
Eduardo A. Inouye <98117951+EduEAI@users.noreply.github.com>
2022-05-03 15:25:51 -0300

    aula 8

---
+++ Curso em video ex/ex014.py
@@ -0,0 +1,3 @@
+c=float(input('quantos graus está? '))
+f=((c*9)/5)+32
+print('{} °C graus é equivalente a {} °F graus '.format(c,f))
---
+++ Curso em video ex/ex015.py
@@ -0,0 +1,5 @@
+d=int(input('Quantos dias o carro foi alugado? '))
+km=float(input('Quantos Km você rodou ? km: '))
+vd=d*60
+vpg=km*0.15+vd
+print('O total a pagar é R$',vpg)
