c50d33589b1d0508bee3065557cb48f0ecf026e4
Eduardo A. Inouye <98117951+EduEAI@users.noreply.github.com>
2022-05-23 13:54:54 -0300

    UP EX019

---
+++ Curso em video ex/ex016.py
@@ -0,0 +1,5 @@
+from math import trunc
+
+num = float(input('Qual numero real: '))
+int= trunc(num)
+print('o numero {} tem a parte inteira {}'.format(num,int))
---
+++ Curso em video ex/ex017.py
@@ -0,0 +1,4 @@
+ca = float(input('Qual cateto adijacente? '))
+co = float(input('Qual cateto oposto? '))
+hi = ((ca ** 2) + (co ** 2))/2
+print ('A hipotenusa de {} e {} é igual a {}'.format(ca, co, hi))
